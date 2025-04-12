5cf6f38a53c3330a881ce68a5aad369017bc8812
mostafa meerzad <mostafameerzad@gmail.com>
2025-04-12 04:55:14 +0000

    Save progress: 650.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -2,13 +2,15 @@
 
 echo -e '\n~~~~~ Bike Rental Shop ~~~~~\n'
 
-MAIN_MENU(){
+MAIN_MENU() {
 
   echo 'How may I help you?'
 
   echo -e '\n1. Rent a bike
 2. Return a bike
 3. Exit'
+
+read MAIN_MENU_SELECTION
 }
 
 MAIN_MENU
