f1419e9ed41d00d9338ba2c405bcb08cc5feb378
mostafa meerzad <mostafameerzad@gmail.com>
2025-04-14 04:52:35 +0000

    Save progress: 1210.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -53,10 +53,12 @@ RENT_MENU() {
       BIKE_AVAILABILITY=$($PSQL "SELECT available FROM bikes WHERE bike_id = $BIKE_ID_TO_RENT AND available = true")
       
       echo $BIKE_AVAILABILITY
+
       # if not available
-
+      if [[ -z $BIKE_AVAILABILITY ]]
+      then
       # send to main menu
-
+      fi
     fi
   fi
 }
